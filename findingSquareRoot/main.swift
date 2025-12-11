2436fd1165c6c7da951ed4384742021c54064e5e
ZelalDev <zelall2222@gmail.com>
2025-12-11 20:33:12 +0300

    code created

--- findingSquareRoot/main.swift
+++ findingSquareRoot/main.swift
@@ -7,5 +7,36 @@
 
 import Foundation
 
-print("Hello, World!")
+enum errorTypes:Error
+{
+    case outOfBonds
+    case noRoot
+}
 
+func findinSquareRoot(_ number: Int)throws ->Int
+{
+    if(number<1 || number>10_000)
+    {
+        throw errorTypes.outOfBonds
+    }
+    for i in 1...100
+    {
+        if i*i == number
+        {
+            return i
+        }
+    }
+    throw errorTypes.noRoot
+}
+
+
+do{
+    let squareRoot = try findinSquareRoot(345)
+    print("the square root is \(squareRoot)")
+}
+catch errorTypes.outOfBonds{
+    print("the number you gave is out of bonds")
+}
+catch errorTypes.noRoot{
+    print("no root has found")
+}
